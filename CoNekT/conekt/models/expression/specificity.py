da8d21453832f0e5c4b4d87510811ce1f9aeaa16
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2023-09-23 14:24:48 -0300

    Updated specificity calculation functions

--- CoNekT/conekt/models/expression/specificity.py
+++ CoNekT/conekt/models/expression/specificity.py
@@ -32,43 +32,12 @@ class ExpressionSpecificityMethod(db.Model):
     def __repr__(self):
         return str(self.id) + ". " + self.description + ' [' + self.species.name + ']'
 
-    @staticmethod
-    def calculate_specificities(species_id, description, remove_background=False):
-        """
-        Function that calculates condition specificities for each profile. No grouping is applied, each condition is
-        used as is
-
-        :param species_id: internal species ID
-        :param description: description for the method to determine the specificity
-        :param remove_background: when true the lowest value of each profile is substracted from all values (can be
-        off use with noisy data derived from microarrays.
-        """
-
-        conditions = []
-
-        # get profile from the database (ORM free for speed)
-        profiles = db.engine.execute(db.select([ExpressionProfile.__table__.c.id, ExpressionProfile.__table__.c.profile]).
-                                     where(ExpressionProfile.__table__.c.species_id == species_id)
-                                     ).fetchall()
-
-        # detect all conditions
-        for profile_id, profile in profiles:
-            profile_data = json.loads(profile)
-            for condition in profile_data['order']:
-                if condition not in conditions:
-                    conditions.append(condition)
-
-        # convert list into dictionary and run function
-        conditions_dict = {k: k for k in conditions}
-        return ExpressionSpecificityMethod.calculate_tissue_specificities(species_id, description, conditions_dict, conditions, remove_background=remove_background)
 
     @staticmethod
-    def calculate_tissue_specificities(species_id, description, condition_to_tissue, order, remove_background=False, use_max=True):
+    def calculate_specificities(species_id, description, remove_background=False, use_max=True):
         """
-        Function calculates tissue specific genes based on the expression conditions. A dict is required to link
-        specific conditions to the correct tissues. This also allows conditions to be excluded in case they are
-        unrelated with a specific tissue.
-
+        Function calculates condition and tissue specific genes based on the expression conditions. 
+        This also allows conditions to be excluded in case they are unrelated with a specific tissue.
 
         :param species_id: internal species ID
         :param description: description for the method to determine the specificity
@@ -82,17 +51,21 @@ class ExpressionSpecificityMethod(db.Model):
         new_method.species_id = species_id
         new_method.description = description
         new_method.menu_order = 0
-        tissues = []
-        for c in order:
-            if c in condition_to_tissue.keys():
-                v = condition_to_tissue[c]
-                if v not in tissues:
-                    tissues.append(v)
+        
+        condition_to_tissue = {}
 
         # get profile from the database (ORM free for speed)
         profiles = db.engine.execute(db.select([ExpressionProfile.__table__.c.id, ExpressionProfile.__table__.c.profile]).
                                      where(ExpressionProfile.__table__.c.species_id == species_id)
                                      ).fetchall()
+
+        # detect all conditions
+        for profile_id, profile in profiles:
+            profile_data = json.loads(profile)
+            for condition, po in profile_data['data']['PO_class'].items():
+                condition_to_tissue[condition] = po
+
+        tissues = list(sorted(set(profile_data['data']['PO_class'].values())))
 
         new_method.conditions = json.dumps(tissues)
 
@@ -108,19 +81,16 @@ class ExpressionSpecificityMethod(db.Model):
             profile_means = {}
             for t in tissues:
                 values = []
-                means = []
-                valid_conditions = [k for k in profile_data['data'] if k in condition_to_tissue and condition_to_tissue[k] == t]
-                for k, v in profile_data['data'].items():
+                valid_conditions = [k for k in profile_data['data']['TPM'].keys() if k in condition_to_tissue and condition_to_tissue[k] == t]
+
+                for k, v in profile_data['data']['TPM'].items():
                     if k in valid_conditions:
-                        values += v
-                        means.append(mean(v))
-
-                if not use_max:
-                    profile_means[t] = mean(values) if len(values) > 0 else 0
-                else:
-                    profile_means[t] = max(means) if len(means) > 0 else 0
-
-            # substract minimum value to remove background
+                        if profile_data['data']['PO_class'][k] == t:
+                            values = values + [v]
+                
+                profile_means[t] = mean(values)
+            
+                        # substract minimum value to remove background
             # experimental code !
             if remove_background:
                 minimum = min([v for k, v in profile_means.items()])
@@ -130,8 +100,8 @@ class ExpressionSpecificityMethod(db.Model):
 
             # determine spm score for each condition
             profile_specificities = []
-            profile_tau = tau([v for _, v in profile_means.items()])
-            profile_entropy = entropy_from_values([v for _, v in profile_means.items()])
+            profile_tau = tau([v for v in profile_data['data']['TPM'].values()])
+            profile_entropy = entropy_from_values([v for v in profile_data['data']['TPM'].values()])
 
             for t in tissues:
                 score = expression_specificity(t, profile_means)
