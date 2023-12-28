59bdb3bc272a8b0255a241319d3f2c18a47eec8e
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2023-12-28 14:53:57 -0300

    probe string/varchar length matches sequence model/table

--- CoNekT/conekt/models/relationships/sequence_cluster.py
+++ CoNekT/conekt/models/relationships/sequence_cluster.py
@@ -6,7 +6,7 @@ class SequenceCoexpressionClusterAssociation(db.Model):
     __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
-    probe = db.Column(db.String(50), index=True)
+    probe = db.Column(db.String(80), index=True)
     sequence_id = db.Column(db.Integer, db.ForeignKey('sequences.id', ondelete='CASCADE'))
     coexpression_cluster_id = db.Column(db.Integer, db.ForeignKey('coexpression_clusters.id', ondelete='CASCADE'))
 
